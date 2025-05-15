ef06d353d2e63d0dd2b2a076f73648f4841ac235
root <root@localhost.localdomain>
2025-05-15 11:05:37 +0900

    middle backup

---
+++ by_date/lab0515/terraform01/main.tf
@@ -0,0 +1,35 @@
+resource "openstack_compute_instance_v2" "basic" {
+  name              = "cirros1"
+  image_id          = "0e05e5d9-2ea3-4ef5-935e-90fdc6acfb06"
+  flavor_id         = "6"
+  security_groups   = ["cirrossg"]
+
+  network {
+    name = "mynet"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# 인스턴스 사설 주소 = 매핑 =  공인주소(floating IP)
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "extnet"
+}
+
+# 위에서 발행한 공인주소를 특정 인스턴스(cirros1)와 매핑핑
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = openstack_networking_floatingip_v2.fip_1.address
+  # 공인 주소
+  instance_id = openstack_compute_instance_v2.basic.id
+  # 어떤 인스턴스의
+  fixed_ip    = openstack_compute_instance_v2.basic.network.0.fixed_ip_v4
+  # 어떤 사설주소(랜카드)와 매핑할 것인지
+}
+
+# 매핑 결과 확인 
+# 다 되고 공인IP 주소 output되게
+output "public-IP" {
+  value = openstack_networking_floatingip_v2.fip_1.address
+}
---
+++ by_date/lab0515/terraform01/securitygroup.tf
@@ -0,0 +1,34 @@
+resource "openstack_networking_secgroup_v2" "cirrossg" {
+  name        = "cirrossg"
+  description = "My neutron security group"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "SSH" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.cirrossg.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "http" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.cirrossg.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ICMP" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 0
+  port_range_max    = 0
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.cirrossg.id
+}

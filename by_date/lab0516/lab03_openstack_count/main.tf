f0fc878ff38d3a6f9f75c04dee4e67a3b86ea4e0
root <root@localhost.localdomain>
2025-05-16 16:23:41 +0900

    ohmagod

--- by_date/lab0516/lab03_openstack_count/main.tf
+++ by_date/lab0516/lab03_openstack_count/main.tf
@@ -1,1 +1,8 @@
+resource "openstack_compute_instance_v2" "cirros" {
+  name              = "cirros-${count.index}"
+  flavor_id         = "1"
+  security_groups   = [ "default" ]
+  count             = 2
+}
+
 0e05e5d9-2ea3-4ef5-935e-90fdc6acfb06

8c2d732c00665f6980fe122614a1d6951845c8be
root <root@localhost.localdomain>
2025-05-16 15:45:07 +0900

    fix

--- by_date/lab0516/lab02/main.tf
+++ by_date/lab0516/lab02/main.tf
@@ -2,12 +2,12 @@ resource "aws_instance" "server" {
   ami = "ami-0f61efb6cfbcc18a4"
   instance_type = "t3.micro"
   count = 2
-  
+
   tags = {
     Name = "server-${count.index}"
   }
 }
 
 output "server_name" {
-    value= aws_instance.server.tags.Name
+    value= aws_instance.server.*.tags.Name
 }
---
+++ by_date/lab0516/lab03_openstack_count/provider.tf
@@ -0,0 +1,19 @@
+# Define required providers
+terraform {
+required_version = ">= 1.0.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "1.54.1"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "test123"
+  auth_url    = "http://211.183.3.10:5000/v3"
+  region      = "RegionOne"
+}

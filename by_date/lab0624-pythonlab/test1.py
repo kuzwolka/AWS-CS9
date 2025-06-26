8815bdcaf618d68ff67048e70d0401111d3d10e6
kuzwolka <kuzwolka@gmail.com>
2025-06-26 16:18:52 +0900

    python

--- by_date/lab0624-pythonlab/test1.py
+++ by_date/lab0624-pythonlab/test1.py
@@ -1,3 +1,9 @@
-import boto3
+import boto3, json
 
-boto3.client('ec2',region_name='ap-northeast-2')
+ec2 = boto3.client('ec2',region_name='ap-northeast-2')
+bedrock = boto3.client('bedrock',region_name='ap-northeast-2')
+
+response = ec2.describe_vpcs()
+response2 = bedrock.list_foundation_models()
+
+print(json.dumps(response2, indent=2))
